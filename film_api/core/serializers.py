9c21649d8b791a9e438ed61d7f2f24f82a73a685
Rahul Rimal <genuinetechnology9@gmail.com>
2022-10-23 22:55:03 +0545

    Resolved user create and login bug of custom user

--- film_api/core/serializers.py
+++ film_api/core/serializers.py
@@ -10,10 +10,13 @@ class UserCreateSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password']
 
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'], email=validated_data['email'], password=make_password(validated_data['password']))
-        return user
+    # def create(self, validated_data):
+    #     user = User.objects.create(
+    #         username=validated_data['username'], email=validated_data['email'], password=make_password(validated_data['password']))
+    #     return user
+
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
 
 
 class UserSerializer(serializers.ModelSerializer):
